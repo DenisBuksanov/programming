1c5fe166d67bd8e340cee4ef09cd5e9a2738c7b9
DenisBuksanov <denisbuksanov@gmail.com>
2025-12-19 09:56:35 +0700

    Update laba 7.c

--- laba 7.c
+++ laba 7.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
 int main() {
     char text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-    char new_text[1000]; 
-    int i = 0; 
-    int j = 0; 
-    
-    new_text[j] = '?';
-    j++;
-    while (text[i] != '\0') {
-        if (text[i] == ' ') {
-            new_text[j] = ' ';
-            j++;
-            new_text[j] = '?';
-            j++;
-        } else {
-            new_text[j] = text[i];
-            j++;
+    int len = strlen(text);
+    int end = len;
+    char out[1000];
+    int k = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        if (text[i] == ' ' || i == 0) {
+            int start = i;
+            if (i == 0) 
+            start = -1;
+            for (int j = start + 1; j < end; j++) {
+                out[k] = text[j];
+                k++;
+            }
+            if (i > 0) {
+            out[k] = ' ';
+            k++;
+            }
+            end = i;
         }
-        i++; 
     }
-    printf("%s\n", new_text);
+
+    printf("%s\n", out);
     return 0;
 }
